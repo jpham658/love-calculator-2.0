c2e011c51216fcb8809b26e8e4a1241f94ce15d9
r4chelpham <r4chelpham@gmail.com>
2023-04-01 18:06:52 +0100

    Still testing calculator

--- calculator.py
+++ calculator.py
@@ -1,22 +1,19 @@
 import random
-#from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 
-randomNumber = random.randint(0, 100)
+random_number = random.randint(0, 100)
+
+with open('calculator-display.html', 'r') as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, 'html.parser')
+
+span = soup.find("span", id='love-score')
+span.string = f"Your random number is: {random_number}"
+
+# Print the contents of the span before and after the modification
+print(f"Before modification: {span}")
+print(f"After modification: {span}")
 
 with open('calculator-display.html', 'w') as file:
-
-    # Write the HTML code to display the random number
-    file.write("<html><body><h1>Your love score is: {randomNumber}</h1></body></html>")
-
-
-#with open('calculator-display.html') as file:
-   # html = file.read()
-
-#soup = BeautifulSoup(html, 'lxml')
-
-#span = soup.find('span', id = 'love-score')
-#span.string = str(randomNumber)
-
-#with open('calculator-display.html', 'w') as file:
-   # file.write(str(soup))
-
+    file.write(soup.prettify())
