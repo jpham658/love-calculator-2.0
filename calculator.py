630c7af9ff3cce36f81ef7e477b213e8a9e4f3da
r4chelpham <r4chelpham@gmail.com>
2023-04-01 17:39:02 +0100

    Testing Python file

--- calculator.py
+++ calculator.py
@@ -1,16 +1,22 @@
 import random
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 
 randomNumber = random.randint(0, 100)
 
-with open('calculator-display.html') as file:
-    html = file.read()
-
-soup = BeautifulSoup(html, 'lxml')
-
-span = soup.find('span', id = 'love-score')
-span.string = str(randomNumber)
-
 with open('calculator-display.html', 'w') as file:
-    file.write(str(soup))
+
+    # Write the HTML code to display the random number
+    file.write(f"<html><body><h1>Your love score is: {randomNumber}</h1></body></html>")
+
+
+#with open('calculator-display.html') as file:
+   # html = file.read()
+
+#soup = BeautifulSoup(html, 'lxml')
+
+#span = soup.find('span', id = 'love-score')
+#span.string = str(randomNumber)
+
+#with open('calculator-display.html', 'w') as file:
+   # file.write(str(soup))
 
